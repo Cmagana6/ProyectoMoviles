8bc1d1ba09055acac71de9515917524a3cb8422e
DEguizabal <00388913@uca.edu.sv>
2018-07-06 21:11:08 -0600

    se valido el darle back a la aplicacion

--- app/src/main/java/com/example/uno/proyectomoviles/GameActivity.java
+++ app/src/main/java/com/example/uno/proyectomoviles/GameActivity.java
@@ -1,5 +1,8 @@
 package com.example.uno.proyectomoviles;
 
+import android.app.AlertDialog;
+import android.content.DialogInterface;
+import android.content.Intent;
 import android.graphics.Point;
 import android.os.Bundle;
 import android.os.PersistableBundle;
@@ -40,5 +43,31 @@ public class GameActivity  extends AppCompatActivity{
     protected void onResume() {
         super.onResume();
         gameView.resume();
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Esta seguro que desea salir?")
+                .setCancelable(false)
+                .setPositiveButton("Si", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+
+                        GameView.stopMusic();
+                        Intent startMain = new Intent(Intent.ACTION_MAIN);
+                        startMain.addCategory(Intent.CATEGORY_HOME);
+                        startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                        startActivity(startMain);
+                        finish();
+                    }
+                })
+                .setNegativeButton("No", new DialogInterface.OnClickListener() {
+                    public void onClick(DialogInterface dialog, int id) {
+                        dialog.cancel();
+                    }
+                });
+        AlertDialog alert = builder.create();
+        alert.show();
+
     }
 }
