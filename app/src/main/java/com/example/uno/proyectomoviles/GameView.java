44a23d14853820fc6b84853c04cdf78b08c16002
DEguizabal <00388913@uca.edu.sv>
2018-07-06 18:12:57 -0600

    Se agregaron sonidos y puntaje y un poco la estetica

--- app/src/main/java/com/example/uno/proyectomoviles/GameView.java
+++ app/src/main/java/com/example/uno/proyectomoviles/GameView.java
@@ -1,10 +1,13 @@
 package com.example.uno.proyectomoviles;
 
 import android.content.Context;
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Rect;
+import android.media.MediaPlayer;
 import android.view.MotionEvent;
 import android.view.SurfaceHolder;
 import android.view.SurfaceView;
@@ -23,10 +26,30 @@ public class GameView extends SurfaceView implements Runnable{
 
     private Player player;
 
+    int screenX;
+    //context para usar en onTouchEvent para navegar entre GameActivity a MainActivity
+    Context context;
+
+    int score;
+
+    int highScore[] = new int[4];
+
+    //para guardar High Scores
+    SharedPreferences sharedPreferences;
+
+    int countMises;
+    //indica si un enemigo entro a la pantalla de juego
+    boolean flag;
+
+    //indica Game Over
+    private boolean isGameOver;
+
     //Estos objetos se utilizaran para dibujar la superficie
     private Paint paint;
     private Canvas canvas;
     private SurfaceHolder surfaceHolder;
+
+    private Enemy enemies;
 
     private Friend friend;
 
@@ -37,6 +60,10 @@ public class GameView extends SurfaceView implements Runnable{
     //definiendo el objeto boom
     private Boom boom;
 
+    //objetos para configurar la musica y sonidos de fondo
+    static MediaPlayer gameOnsound;
+    final MediaPlayer killedEnemysound;
+    final MediaPlayer gameOversound;
 
     //constructor
 
@@ -51,6 +78,8 @@ public class GameView extends SurfaceView implements Runnable{
         surfaceHolder = getHolder();
         paint = new Paint();
 
+        this.context = context;
+
         //añadiendo estrellas
         int starNums = 100;
         for (int i = 0; i < starNums; i++){
@@ -58,13 +87,37 @@ public class GameView extends SurfaceView implements Runnable{
             stars.add(s);
         }
 
+        enemies = new Enemy(context, screenX, screenY);
+
         //iniciando objeto Boom
         boom = new Boom(context);
 
         //iniciando el objeto de la clase Friend
         friend = new Friend(context, screenX, screenY);
 
+        score = 0;
 
+        countMises = 0;
+
+        this.screenX = screenX;
+
+        isGameOver = false;
+
+        sharedPreferences = context.getSharedPreferences("SHAR_PREF_NAME", Context.MODE_PRIVATE);
+
+        //se inicializan los valores del arreglo de puntaje
+        highScore[0] = sharedPreferences.getInt("score1", 0);
+        highScore[1] = sharedPreferences.getInt("score2", 0);
+        highScore[2] = sharedPreferences.getInt("score3", 0);
+        highScore[3] = sharedPreferences.getInt("score4", 0);
+
+        //inicializan los valores de los sonidos
+        gameOnsound = MediaPlayer.create(context, R.raw.gameon);
+        killedEnemysound = MediaPlayer.create(context, R.raw.killedenemy);
+        gameOversound = MediaPlayer.create(context, R.raw.gameover);
+
+        //inicia la musica que va a sonar en el juego
+        gameOnsound.start();
     }
 
     @Override
@@ -82,6 +135,9 @@ public class GameView extends SurfaceView implements Runnable{
     }
 
     private void update() {
+        //aumenta el puntaje entre mas pasa el tiempo
+        score++;
+
         //actualiza la posicion del jugador
         player.update();
 
@@ -92,10 +148,13 @@ public class GameView extends SurfaceView implements Runnable{
         for (Star s : stars){
             s.update(player.getSpeed());
         }
+        //pone la vandera en true cuando los enemigo aparecen en pantalla
+        if (enemies.getX() == screenX){
+            flag = true;
+        }
 
 
         enemies.update(player.getSpeed());
-
             //si la colision ocurre con Player
             if(Rect.intersects(player.getDetectCollision(), enemies.getDetectCollision())){
 
@@ -103,11 +162,81 @@ public class GameView extends SurfaceView implements Runnable{
                 boom.setX(enemies.getX());
                 boom.setY(enemies.getY());
 
+                //inicializa el sonido de colision entre el jugador y el enemigo
+                killedEnemysound.start();
+
                 //moviendo al enemigo afuera del borde izquierdo
                enemies.setX(-200);
+            }else {//cuando faya el jugador
+                //si el enemigo acaba de entrar a la pantalla
+                if (flag){
+                    //si la coordenadas en x son iguales a las coordenadas en y del enemigo
+                    if (player.getDetectCollision().exactCenterX() >= enemies.getDetectCollision().exactCenterX()){
+                        countMises++;
+                        //se pone la bandera en falso para que el else solo se ejecute cuando el enemigo este en pantalla
+                        flag = false;
+
+                        //si se falla 3 veces se pierde el juego
+                        if (countMises == 3){
+                            //se ponen las banderas en su valor correspondiente
+                            playing = false;
+                            isGameOver = true;
+
+                            gameOnsound.stop();
+                            gameOversound.start();
+
+                            //asigna el puntaje al arreglo
+                            for (int i=0; i<4; i++){
+                                if (highScore[i]<score){
+                                    final int finalI = i;
+                                    highScore[i] = score;
+                                    break;
+                                }
+                            }
+                            //guarda el puntaje por shared preferences
+                            SharedPreferences.Editor e = sharedPreferences.edit();
+
+                            for (int i=0; i<4; i++){
+                                int j = i+1;
+                                e.putInt("puntaje"+j, highScore[i]);
+                            }
+                            e.apply();
+                        }
+                    }
+                }
             }
 
           friend.update(player.getSpeed());
+            //revisa si hay collision
+        if (Rect.intersects(player.getDetectCollision(), friend.getDetectCollision())){
+            // muestra explosion en la colision
+            boom.setX(friend.getX());
+            boom.setY(friend.getY());
+            //se detiene el juego
+            playing = false;
+            isGameOver = true;
+
+            gameOnsound.stop();
+            gameOversound.start();
+
+            //asignando el puntaje al arreglo
+            for (int i=0;i<4;i++){
+                if (highScore[i]<score){
+                    final int finalI = i;
+                    highScore[i] = score;
+                    break;
+                }
+            }
+
+            //guardando el puntaje
+            SharedPreferences.Editor e = sharedPreferences.edit();
+
+            for (int i=0;i<4;i++){
+                int j = i=1;
+                e.putInt("puntaje"+j,highScore[i]);
+            }
+            e.apply();
+        }
 
     }
 
@@ -120,6 +249,7 @@ public class GameView extends SurfaceView implements Runnable{
             canvas.drawColor(Color.BLACK);
             //asignando el color de las estrellas
             paint.setColor(Color.WHITE);
+            paint.setTextSize(20);
 
             //dibujando las estrellas
             for(Star s : stars){
@@ -130,6 +260,11 @@ public class GameView extends SurfaceView implements Runnable{
             canvas.drawBitmap(player.getBitmap(),
                     player.getX(), player.getY(), paint);
 
+            canvas.drawBitmap(enemies.getBitmap(), enemies.getX(), enemies.getY(), paint);
+
+            //dibuja el puntaje en la pantalla de juego
+            paint.setTextSize(30);
+            canvas.drawText("Puntaje"+score,100,50,paint);
 
             //dibujando imagen boom
             canvas.drawBitmap(
@@ -146,6 +281,15 @@ public class GameView extends SurfaceView implements Runnable{
                     friend.getY(),
                     paint
             );
+
+            //pone game over cuando se pierde
+            if (isGameOver){
+                paint.setTextSize(150);
+                paint.setTextAlign(Paint.Align.CENTER);
+
+                int yPos = (int) ((canvas.getHeight() / 2) - ((paint.descent() + paint.ascent())/2));
+                canvas.drawText("GAME OVER", canvas.getWidth() / 2, yPos, paint);
+            }
 
             //Desbloqueando el canvas
             surfaceHolder.unlockCanvasAndPost(canvas);
@@ -179,6 +323,11 @@ public class GameView extends SurfaceView implements Runnable{
 
     }
 
+    //Para la musica al salir
+    public static void stopMusic(){
+        gameOnsound.stop();
+    }
+
 
 
     @Override
@@ -191,6 +340,12 @@ public class GameView extends SurfaceView implements Runnable{
         player.setBoosting();
         break;
 
+        }
+        //si se acabo el juego un tap a la pantalla nos regresa a la pantalla de inicio
+        if (isGameOver){
+            if (motionEvent.getAction() == MotionEvent.ACTION_DOWN){
+                context.startActivity(new Intent(context, MainActivity.class));
+            }
         }
         return true;
     }
--- app/src/main/java/com/example/uno/proyectomoviles/MainActivity.java
+++ app/src/main/java/com/example/uno/proyectomoviles/MainActivity.java
@@ -1,6 +1,8 @@
 package com.example.uno.proyectomoviles;
 
 import android.app.Activity;
+import android.app.AlertDialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
 import android.support.v7.app.AppCompatActivity;
@@ -8,9 +10,10 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.ImageButton;
 
-public class MainActivity extends AppCompatActivity implements View.OnClickListener{
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
 
     private ImageButton buttonPlay;
+    private ImageButton buttonScore;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -22,15 +25,46 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         //Obteniendo el boton
         buttonPlay = (ImageButton) findViewById(R.id.buttonPlay);
+        buttonScore = (ImageButton) findViewById(R.id.buttonScore);
 
         //Agregando un listener al boton
         buttonPlay.setOnClickListener(this);
+        buttonScore.setOnClickListener(this);
     }
 
     @Override
     public void onClick(View v) {
-        //Iniciando game activity
-        startActivity(new Intent(MainActivity.this, GameActivity.class));
+        if (v == buttonPlay) {
+            //Iniciando game activity
+            startActivity(new Intent(MainActivity.this, GameActivity.class));
+        }
+        if (v == buttonScore) {
+            //iniciando highscore activity
+            startActivity(new Intent(MainActivity.this, HighScore.class));
+        }
+    }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage("Esta segura que deasea salir del juego?").setCancelable(false).setPositiveButton("Si", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                GameView.stopMusic();
+                Intent startMain = new Intent(Intent.ACTION_MAIN);
+                startMain.addCategory(Intent.CATEGORY_HOME);
+                startMain.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+                startActivity(startMain);
+                finish();
+            }
+        }).setNegativeButton("No", new DialogInterface.OnClickListener() {
+            @Override
+            public void onClick(DialogInterface dialog, int which) {
+                dialog.cancel();
+            }
+        });
+        AlertDialog alert = builder.create();
+        alert.show();
     }
 
 }
